6f3974a02fab95aa6b96e1f9b304754367a6c13b
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-04 06:37:23 +0100

    rebase head to remote

---
+++ 0x00-ES6_basic/2-main.js
@@ -0,0 +1,6 @@
+import getNeighborhoodsList from "2-arrow.js"
+
+const neighborhoodList = new getNeighborhoodsList();
+
+const res = neighborhoodList.addNeighborhood("Noe Valley");
+console.log(nes);
--- 0x00-ES6_basic/README.md
+++ 0x00-ES6_basic/README.md
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 # 0x00. ES6 Basics
+=======
+# 0x00. ES6 Basics 
+>>>>>>> 9a54eab (1 to 9)
