316a25bf37e984a634de3bf1f6a91038f8bef637
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-05 10:08:04 +0100

    attaching handlers to promise

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,6 @@
+export default function handleResponseFromApi(promise) {
+  promise.then(() => ({
+    status: 200,
+    body: 'Success',
+  })).catch(() => new Error()).then(() => console.log('Got a response from the API'));
+}
