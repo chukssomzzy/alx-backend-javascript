44ded8be865e565756159c934ad0083463a7b8d6
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-05 10:15:54 +0100

    changed return from 'Success' to 'success'

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromApi(promise) {
   promise.then(() => ({
     status: 200,
-    body: 'Success',
+    body: 'success',
   })).catch(() => new Error()).finally(() => console.log('Got a response from the API'));
 }
