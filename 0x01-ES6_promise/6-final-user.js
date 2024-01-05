e2679b7379b6ae2d6cd8dd0e6e135d92fb7546c2
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-05 14:07:13 +0000

    did not map the array

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,5 +4,6 @@ import uploadPhoto from './5-photo-reject';
 export default function handleUserSignUp(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => ({ status: res.status, value: res.value }));
+    .then((res) => res
+      .map((res) => ({ status: res.status, value: res.value ? res.value : res.reason })));
 }
