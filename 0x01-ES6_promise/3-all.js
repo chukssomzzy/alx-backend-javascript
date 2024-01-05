ef1ebd2dbe19e3979cbc803ea2eb3e339c39d8fa
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-05 11:12:00 +0100

    return promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise
+  return Promise
     .all([uploadPhoto(), createUser()]).then((arr) => {
-    console.log(arr[0].body, arr[1].firstName, arr[1].lastName);
+      console.log(arr[0].body, arr[1].firstName, arr[1].lastName);
     })
-    .catch(() => console.log("Signup system offline"));
+    .catch(() => console.log('Signup system offline'));
 }
