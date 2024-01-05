bae72fe08bbd51510498b3f1d9e4b3ae176d6e25
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-05 15:32:53 +0000

    convert reason to string

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -7,7 +7,7 @@ export default function handleUserSignUp(firstName, lastName, fileName) {
     .then((results) => {
       const resArray = results.map((result) => ({
         status: result.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+        value: result.value ? result.value : String(result.reason),
       }));
       return resArray;
     });
