36a7a3576f5bcf1cde4486f582a831f480d913ad
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-06 16:06:11 +0100

    toStringTag

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,38 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._name = name;
+    if (typeof code !== 'string') {
+      throw new Error('Code must be of type string');
+    }
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new Error('Code must be a string');
+    }
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
