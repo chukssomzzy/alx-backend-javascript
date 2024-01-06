0c84e8ec9048d71e1102915c9cb64bea6bb363ba
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-06 08:58:20 +0100

    setter and gettera

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,65 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new Error('Name must be a String');
+    }
+
+    if (typeof length  === 'number') {
+      this._length = length;
+    } else {
+      throw new Error('Length must be a number');
+    }
+
+    if (students instanceof Array) {
+      for (const student of students) {
+        if (typeof student !== 'string') {
+          throw new Error('Student must be an array of string');
+        }
+      }
+      this._students = students;
+    } else {
+      throw new Error('Student must be an array of string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new Error('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (students instanceof Array) {
+      for (const student in students) {
+        if (typeof student !== 'string') {
+          throw new Error('Students must be an array of string');
+        }
+      }
+      this._students = students;
+    } else {
+      throw new Error('Student must be an array of strings');
+    }
+  }
+}
