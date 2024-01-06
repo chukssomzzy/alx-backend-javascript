28f9661e0647c9743f4af51ec5328549d6562b1d
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-06 21:06:56 +0100

    super.constructor

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,20 @@
+import Car from './10-car';
+
+export default class EvCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(range) {
+    this._range = range;
+  }
+
+  cloneCar() {
+    return new super.constructor();
+  }
+}
