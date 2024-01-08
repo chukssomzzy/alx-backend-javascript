302ae82316258909861e72b53dbd6c5ee411f434
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-08 18:29:36 +0100

    es6 data manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents()
+{
+  const person1 = {
+    name: "Guillaume",
+    id: 1,
+    location: "San Francisco"
+  }
+
+  const person2 = {
+    name: "James",
+    id: 2,
+    location: "Columbia"
+  }
+
+  const person3 = {
+    name: "Serena",
+    id: 5,
+    location: "San Francisco"
+  }
+  return [person1, person2, person3]
+}
