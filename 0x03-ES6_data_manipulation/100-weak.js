5bb8f63c8cfed14ec42d5c2a8ef5b60ee43b3d2f
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-09 22:32:52 +0100

    error handled

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,12 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  const calls = weakMap.get(endpoint) || 0;
-  weakMap.set(endpoint, calls + 1);
+  if (endpoint instanceof Object) {
+    const calls = weakMap.get(endpoint) || 0;
+    weakMap.set(endpoint, calls + 1);
 
-  if (calls >= 5) {
-    throw new Error('Endpoint load is high');
+    if (calls >= 5) {
+      throw new Error('Endpoint load is high');
+    }
   }
 }
