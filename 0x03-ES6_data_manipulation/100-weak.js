099888c690978ea3c87b2dce0263227b285a585d
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-09 22:29:38 +0100

    weak ref

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,10 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  const calls = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, calls + 1);
+
+  if (calls >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
