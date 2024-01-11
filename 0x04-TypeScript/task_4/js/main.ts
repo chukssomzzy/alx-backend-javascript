f28411c1a25cc58fb4eb559092d24b7888d7ad9d
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-11 15:11:50 +0000

    Brand convection

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,8 +1,27 @@
 /// <reference path="./subjects/Cpp.ts"> 
 /// <reference path="./subjects/React.ts"> 
 /// <reference path="./subjects/Java.ts">
+/// <reference path="./subjects/Teacher.ts">
 
-export const cpp = Subjects.Cpp; 
-export const java = Subjects.Java;
-export const react = Subjects.React;
-export 
+export const cpp = new Subjects.Cpp(); 
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+export const cTeacher = {
+  firstName: "chukwuma",
+  lastName: "somtochukwu", 
+  experienceTeachingC: 10,
+}
+console.log("c++"); 
+cpp.setTeacher = cTeacher; 
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log("java"); 
+java.setTeacher = cTeacher; 
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log("React");
+react.setTeacher = cTeacher;
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
