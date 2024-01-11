804f1d6d50d8906fcd5ec2deaddf92e3b570c4dd
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-11 06:51:33 +0100

    no typescript error

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,3 +7,41 @@ export interface Teacher {
   [key: string]: any;
 }
 
+export interface Director extends Teacher {
+  numberOfReport: number;
+}
+
+export interface printTeacherFunction {
+  printTeacher(firstName: string, lastName: string): string;
+}
+
+export function printTeacher(firstName: string, lastName: string): string {
+  return firstName[0] + '. ' + lastName;
+}
+
+export interface IStudentClass {
+  workOnHomework(): string; 
+  displayName(): string;
+}
+
+export interface IStudent {
+  new (firstName:string, lastName: string): IStudent;
+}
+
+export class StudentClass implements IStudentClass {
+  firstName: string; 
+  lastName: string; 
+
+  constructor (firstName: string, lastName: string) {
+    this.firstName = firstName; 
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently Working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
