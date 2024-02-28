6edc1765fa260973ee142808bce59dbb53b9a6d6
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-28 16:11:09 +0000

    IMPLEMENTED: a http server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const http = require('http');
+
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port);
+
+module.exports = app;
