02e76678d5f5e65a53ae1b1d4fbd759b42340bf8
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-03-02 21:28:41 +0100

    UPDATED: csv reader to synchroneous

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,12 +1,34 @@
 #!/usr/bin/node
 
+const { readFileSync } = require('fs');
 const http = require('http');
 const { URL } = require('url');
-const countStudents = require('./3-read_file_async');
 
 const dbUrl = process.argv[2];
 const port = 1245;
+const fieldIdx = 3;
+const values = 4;
+const studentName = 0;
 
+function readCSV () {
+  const fileCont = readFileSync(dbUrl, { encoding: 'utf-8' });
+  let students = 0;
+  const studentsByField = {};
+
+  for (const line of fileCont.split('\n').slice(1)) {
+    const splitLine = line.split(',');
+    if (splitLine.length === values) {
+      students += 1;
+
+      if (studentsByField[splitLine[fieldIdx]]) {
+        studentsByField[splitLine[fieldIdx]].push(splitLine[studentName]);
+      } else {
+        studentsByField[splitLine[fieldIdx]] = [splitLine[studentName]];
+      }
+    }
+  }
+  return [studentsByField, students];
+}
 const app = http.createServer((req, res) => {
   const urlObj = new URL(req.url, `http://${req.headers.host}`);
   res.setHeader('Content-Type', 'Text/Plain');
@@ -15,15 +37,21 @@ const app = http.createServer((req, res) => {
     res.statusCode = 200;
     res.end('Hello Holberton School!');
   } else if (urlObj.pathname === '/students' && dbUrl) {
-    res.statusCode = 200;
+    const [studentsByField, students] = readCSV();
+    let i = 0;
+
     res.write('This is the list of our students\n');
-    countStudents(dbUrl)
-      .then(([studentList, numStudent]) => {
-        res.write(`${numStudent}\n`);
-        res.write(studentList.join('\n'));
-        res.end('');
-      });
+    res.write(`Number of students: ${students}\n`);
+    for (const [fields, students] of Object.entries(studentsByField)) {
+      i += 1;
+
+      res.write(`Number of students in ${fields}: ${students.length}. List: ${students.join(', ')}`);
+      if (i < Object.keys(studentsByField).length) {
+        res.write('\n');
+      }
+    }
   }
+  res.end();
 });
 
 app.listen(port);
